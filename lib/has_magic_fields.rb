1ebdc48636c42341d81aae955c4e31b74a9bf503
Uriel Tzel <aero.uriel@gmail.com>
2020-08-25 19:02:12 -0500

    Add zeitwerk

--- lib/has_magic_fields.rb
+++ lib/has_magic_fields.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.do_not_eager_load("#{__dir__}/generators")
+loader.push_dir("#{__dir__}/../app/models")
+loader.setup
+loader.eager_load
+
 require "has_magic_fields/version"
 require "has_magic_fields/extend"
